85d69dcd47c7796c892a17f33b7d8b3afb5623d8
Temich7799 <artemgl9999@gmail.com>
2024-10-15 18:53:56 +0300

    make the random refetching logic

--- src/components/UsersCardsGrid/index.tsx
+++ src/components/UsersCardsGrid/index.tsx
@@ -1,3 +1,4 @@
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { useQuery } from 'react-query';
 
 import UserCard from 'components/UserCard';
@@ -13,8 +14,32 @@ const formatUsersResponse = (response: IUsersResponse): IUserCard[] => {
   }));
 };
 
-const getUsers = async (): Promise<IUserCard[]> => {
-  const res = await fetch('https://randomuser.me/api/?results=10');
+const getRandomIndexesMap = (count: number) => {
+  const indexesMap = new Map<number, number>();
+  while (indexesMap.size < count) {
+    const randomIndex = Math.floor(Math.random() * count);
+    indexesMap.set(randomIndex, randomIndex);
+  }
+  return indexesMap;
+};
+
+const replaceRandomCards = (oldCards: IUserCard[], newCards: IUserCard[]): IUserCard[] => {
+  const indexesToChange = getRandomIndexesMap(newCards.length);
+
+  const updatedCards = oldCards.map((card, index) => {
+    if (indexesToChange.has(index)) {
+      return newCards.pop();
+    }
+    return card;
+  });
+
+  return updatedCards as IUserCard[];
+};
+
+const initialCount = 10;
+
+const getUsers = async (count: number = initialCount): Promise<IUserCard[]> => {
+  const res = await fetch('https://randomuser.me/api/?results=' + count);
   if (!res.ok) {
     throw new Error('Network response was not ok');
   }
@@ -23,14 +48,43 @@ const getUsers = async (): Promise<IUserCard[]> => {
 };
 
 const UsersCardsGrid = () => {
-  const { data, isLoading, error } = useQuery('users', getUsers);
+  const { current } = useRef({
+    count: initialCount,
+  });
 
-  if (isLoading) return <div>Loading...</div>; //TODO: make splash screen
-  if (error) return <div>Error loading cards</div>; //TODO: make error screen
+  const [cards, setCards] = useState<IUserCard[]>([]);
+
+  const onSuccessHandler = useCallback(
+    (newData: IUserCard[]) => {
+      if (cards.length > 0) setCards(replaceRandomCards(cards, newData));
+      else setCards(newData);
+    },
+    [cards]
+  );
+
+  const { isLoading, error, refetch } = useQuery('users', () => getUsers(current.count), {
+    onSuccess: onSuccessHandler,
+  });
+
+  useEffect(() => {
+    const timerId = setTimeout(async () => {
+      try {
+        current.count = Math.floor(Math.random() * 9) + 1;
+        refetch();
+      } catch (err) {
+        console.error('Error fetching new users:', err);
+      }
+    }, 3000);
+
+    return () => clearTimeout(timerId);
+  }, [refetch, current, cards]);
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error loading cards</div>;
 
   return (
     <div className={styles.gridContainer}>
-      {data?.map((card: IUserCard) => (
+      {cards.map((card: IUserCard) => (
         <UserCard
           key={card.id}
           data={card.content}
