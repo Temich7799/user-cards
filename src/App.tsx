64033558235798acf6c18e05d2114d1a686d105f
Temich7799 <artemgl9999@gmail.com>
2024-10-15 17:11:46 +0300

    styles fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { QueryClient, QueryClientProvider } from 'react-query';
 
-import UsersCardsGrid from 'components/UsersCardsGrid';
+import UsersCardsGrid from 'components/CardsGrid';
+
+import styles from './styles.module.scss';
 
 const queryClient = new QueryClient();
 
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      <UsersCardsGrid />
+      <main className={styles.main}>
+        <UsersCardsGrid />
+      </main>
     </QueryClientProvider>
   );
 };
