da6c49a011ab6574ba7a4dacb4950d44480689a2
Temich7799 <artemgl9999@gmail.com>
2024-10-16 12:04:06 +0300

    update styles, provide random colors logic

--- src/components/UserCard/index.tsx
+++ src/components/UserCard/index.tsx
@@ -1,23 +1,35 @@
 import { IUser } from 'ts/models';
+import { getRandomColor } from 'utils/colors';
 
 import styles from './styles.module.scss';
 
 type UserCardProps = { data: IUser };
 
-const UserCard = (props: UserCardProps) => {
+const UserCard = ({ data }: UserCardProps) => {
+  const backgroundColor = getRandomColor();
+
   return (
-    <div className={styles.userCard}>
-      <div className={styles.cardPhoto}>
-        <img
-          src={props.data.picture.large}
-          alt={props.data.name.first}
-        />
+    <div
+      className={styles.userCard}
+      style={{ backgroundColor }}
+    >
+      <div className={styles.cardHeader}>
+        <h2>{`${data.name.first} ${data.name.last}`}</h2>
       </div>
       <div className={styles.cardBody}>
-        <p className={styles.cardText}>
-          {props.data.name.first} {props.data.name.last}
-        </p>
-        <p className={styles.cardText}>Phone: {props.data.phone}</p>
+        <div className={styles.cardDetails}>
+          <p>{data.location.city}</p>
+          <p>{data.phone}</p>
+        </div>
+      </div>
+      <div className={styles.cardFooter}>
+        <div className={styles.cardPhoto}>
+          <img
+            src={data.picture.large}
+            alt={data.name.first}
+          />
+        </div>
+        <button className={styles.viewButton}>View</button>
       </div>
     </div>
   );
---
+++ src/utils/colors.ts
@@ -0,0 +1,3 @@
+export const colors = ['#fce8f0', '#fff5e6', '#f0f8ff', '#e6f7e6', '#f9f2ff', '#fffbeb', '#e8f7ff'];
+
+export const getRandomColor = () => colors[Math.floor(Math.random() * colors.length)];
